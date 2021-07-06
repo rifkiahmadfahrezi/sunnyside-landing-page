731ae540f6986e906584499b2f56227863683631
Rifki ahmad fahrezi <rifkiaf589@gmail.com>
2021-07-06 08:15:26 +0700

    fixing position bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
 const menu = document.querySelector('.navbar-menu')
+const bars = document.querySelectorAll('.menu-bar')
 
 window.addEventListener('click', e => {
-	if (e.target.classList.contains('burger-menu') || e.target.classList.contains('bx-menu') || e.target == menu) {
+	if (e.target.classList.contains('burger-menu') || e.target.classList.contains('menu-bar') || e.target == menu) {
 	 	menu.classList.toggle('active')
+	 	bars.forEach(bar => bar.classList.toggle('active-menu'))
 	}else {
 		menu.classList.remove('active')
+		bars.forEach(bar => bar.classList.remove('active-menu'))
 	}
 })
+

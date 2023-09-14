bd40b9dcd6ea9fd32f6f4c3840c73fdd2b326771
rifkiahmadfahrezi <rifkiaf589@gmail.com>
2023-09-14 13:04:30 +0700

    add loading animation and navbar scroll animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector('.navbar-menu')
 const bars = document.querySelectorAll('.menu-bar')
+const navbar = document.querySelector("#top-navbar")
 
 window.addEventListener('click', e => {
 	if (e.target.classList.contains('burger-menu') || e.target.classList.contains('menu-bar') || e.target == menu) {
@@ -11,3 +12,21 @@ window.addEventListener('click', e => {
 	}
 })
 
+window.addEventListener("scroll", function(e) {
+	if(window.scrollY > 150){
+		navbar.classList.add("scrolled")
+
+	} else{
+		navbar.classList.remove("scrolled")
+	}
+	console.log(navbar.classList.contains("scrolled"))
+})
+
+window.addEventListener("load", () => {
+	const loadingElement = document.querySelector(".load")
+	loadingElement.classList.add("loaded")
+
+	setTimeout(() => {
+		loadingElement.style.display = "none"
+	},500)
+})
